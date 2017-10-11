fb82ff7bfb05b4be164f4d9e14abec31165e3e0f
Dave Kubasiak <david.kubasiak@itron.com>
2017-10-11 16:39:51 -0400

    Refactored as requested
     - changed if/elsif/else statements to case/when statements
    
    [#151556917]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,30 +1,34 @@
 #! /usr/bin/env ruby
 
 def line1(num)
-  if num > 1
-    phrase = "#{num} bottles"
-  elsif num == 1
-    phrase = "#{num} bottle"
-  else
-    phrase = 'No more bottles'
-  end
+  phrase = case num
+    when 1
+      "#{num} bottle"
+    when 0
+      'No more bottles'
+    else
+      "#{num} bottles"
+    end
+
   "#{phrase} of beer on the wall, #{phrase.downcase} of beer."
 end
 
 def line2(num)
   phrase1 = 'Take one down and pass it around, '
-  if num > 1
-    phrase2 = "#{num} bottles of beer on the wall."
-  elsif num == 1
-    phrase2 = '1 bottle of beer on the wall.'
-  elsif num == 0
-    phrase2 = 'no more bottles of beer on the wall.'
-  else
-    phrase1 = 'Go to the store and buy some more, '
-    phrase2 = '99 bottles of beer on the wall.'
+  case num
+    when 2..Float::INFINITY
+      phrase2 = "#{num} bottles of beer on the wall."
+    when 1
+      phrase2 = '1 bottle of beer on the wall.'
+    when 0
+      phrase2 = 'no more bottles of beer on the wall.'
+    else
+      phrase1 = 'Go to the store and buy some more, '
+      phrase2 = '99 bottles of beer on the wall.'
   end
 
   "#{phrase1}#{phrase2}"
+
 end
 
 def sing(num)
