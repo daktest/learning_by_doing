229cd73c721aae588f2e0e0049e1872b9789e22c
Dave Kubasiak <david.kubasiak@itron.com>
2017-10-25 09:04:18 -0400

    Refactored again
     - made suggested changes
    
    [#151556921]

--- objects/television.rb
+++ objects/television.rb
@@ -1,19 +1,20 @@
-#! /usr/bin/env ruby
+#!/usr/bin/env ruby
 
 =begin
 
 Create a new Television with 'new'.  It has channels 3 to 500 and the volume goes from
  0 to 25.
 
- - turn it on or off by calling 'power'; nothing else works unless it's on
+ - check whether it's on or not by calling 'on?'; nothing else responds unless it's on
+ - turn it on or off by calling 'power'.
  - 'channel_up'/'channel_down' will go up or down to the next channel; it will loop when
    reaching the first or last channel.
  - 'channel_set(999)' will jump to the selected channel as long as it's within 3-500.
  - 'channel_swap' will change to whatever previous channel you were on.
  - 'volume_up'/'volume_down' will increase or decrease the volume by 1 until the upper
    or lower limit is reached.
- - 'mute' sets the volume to 0; if the volume is already zero, it sets it to whatever it
-   was before mute was called.
+ - 'mute' sets the volume to 0; if already muted, it sets it to whatever it was before
+    mute was called.
  - 'status' displays the current channel and volume settings.
 
 =end
@@ -27,6 +28,7 @@ class Television
 
   def initialize
     @on = false
+    @muted = false
     @channel = 3
     @prev_channel = 3
     @volume = 0
@@ -39,75 +41,71 @@ class Television
   end
 
   def set_channel(number)
-    if on?
-      if !CHANNEL_RANGE.include?(number)
-        puts "channel #{number} does not exist."
-      else
-        @prev_channel = @channel
-        @channel = number
-      end
-      channel_status
+    return unless on?
+    if !CHANNEL_RANGE.include?(number)
+      puts "channel #{number} does not exist."
+    else
+      @prev_channel = @channel
+      @channel = number
     end
+    channel_status
   end
 
   def channel_up
-    if on?
-      @prev_channel = @channel
-      @channel += 1
-      @channel = CHANNEL_RANGE.first if @channel > CHANNEL_RANGE.last
-      channel_status
-    end
+    return unless on?
+    @prev_channel = @channel
+    @channel += 1
+    @channel = CHANNEL_RANGE.first if @channel > CHANNEL_RANGE.last
+    channel_status
   end
 
   def channel_down
-    if on?
-      @prev_channel = @channel
-      @channel -= 1
-      @channel = CHANNEL_RANGE.last if @channel < CHANNEL_RANGE.first
-      channel_status
-    end
+    return unless on?
+    @prev_channel = @channel
+    @channel -= 1
+    @channel = CHANNEL_RANGE.last if @channel < CHANNEL_RANGE.first
+    channel_status
   end
 
   def channel_swap
-    if on?
-      temp_chan = @prev_channel
-      @prev_channel = @channel
-      @channel = temp_chan
-      channel_status
-    end
+    return unless on?
+    temp_chan = @prev_channel
+    @prev_channel = @channel
+    @channel = temp_chan
+    channel_status
   end
 
   def volume_up
-    if on?
-      @volume += 1 if @volume < VOLUME_RANGE.last
-      volume_status
-    end
+    return unless on?
+    @muted = false
+    @volume += 1 if @volume < VOLUME_RANGE.last
+    volume_status
   end
 
   def volume_down
-    if on?
-      @volume -= 1 if @volume > VOLUME_RANGE.first
-      volume_status
-    end
+    return unless on?
+    @muted = false
+    @volume -= 1 if @volume > VOLUME_RANGE.first
+    volume_status
   end
 
   def mute
-    if on?
-      if @volume == 0
-        @volume = @pre_mute_volume
-      else
-        @pre_mute_volume = @volume
-        @volume = 0
-      end
-      volume_status
+    return unless on?
+    if @muted
+      @volume = @pre_mute_volume
+      @muted = false
+    else
+      @pre_mute_volume = @volume
+      @volume = 0
+      @muted = true
     end
+    volume_status
   end
 
   def status
-    if on?
-      channel_status
-      volume_status
-    end
+    return unless on?
+    channel_status
+    volume_status
   end
 
   private
