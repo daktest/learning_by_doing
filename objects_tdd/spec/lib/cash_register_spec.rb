4ef0e40f1273112d39abb2ea8f2c4854508c8b4c
Dave Kubasiak <david.kubasiak@itron.com>
2017-10-30 14:54:08 -0400

    Made some changes
     - removed `let` and used `subject` as the instance
     - removed extra `expects`
       - left one test with two expects since a total payment should both give change and reset the total
    
    [#152207286]

--- objects_tdd/spec/lib/cash_register_spec.rb
+++ objects_tdd/spec/lib/cash_register_spec.rb
@@ -1,50 +1,44 @@
 require './lib/cash_register.rb'
 
 describe CashRegister do
-  let(:cashregister) { CashRegister.new }
   it 'can be created' do
-    expect(cashregister).to be_a(CashRegister)
+    expect(subject).to be_a(CashRegister)
   end
 
   describe '#total' do
     it 'returns 0.0 by default' do
-      expect(cashregister.total).to eq(0.0)
+      expect(subject.total).to eq(0.0)
     end
   end
 
   describe '#purchase(amount)' do
     it 'should add amount to total' do
-      cashregister.purchase(5.75)
-      expect(cashregister.total).to eq(5.75)
-      cashregister.purchase(4.25)
-      expect(cashregister.total).to eq(10.0)
+      subject.purchase(5.75)
+      subject.purchase(4.25)
+      expect(subject.total).to eq(10.0)
     end
   end
 
   context '#pay(amount) behaviors' do
     it 'should subtract amount from the total' do
-      cashregister.purchase(5.75)
-      expect(cashregister.total).to eq(5.75)
-      cashregister.pay(5.00)
-      expect(cashregister.total).to eq(0.75)
+      subject.purchase(5.75)
+      subject.pay(5.00)
+      expect(subject.total).to eq(0.75)
     end
 
     it 'gives change and sets total to zero if amount > than the total' do
-      cashregister.purchase(5.75)
-      expect(cashregister.total).to eq(5.75)
+      subject.purchase(5.75)
       expect do
-        cashregister.pay(6.00)
+        subject.pay(6.00)
       end.to output("Your change is $0.25\n").to_stdout
-      expect(cashregister.total).to eq(0)
+      expect(subject.total).to eq(0)
     end
 
     it 'if partial payment, gives new total' do
-      cashregister.purchase(5.75)
-      expect(cashregister.total).to eq(5.75)
+      subject.purchase(5.75)
       expect do
-        cashregister.pay(5.00)
+        subject.pay(5.00)
       end.to output("Your new total is $0.75\n").to_stdout
-      expect(cashregister.total).to eq(0.75)
     end
   end
 end
