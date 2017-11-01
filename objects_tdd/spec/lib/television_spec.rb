c21ab143f66282ad28ab1c97e2501a0fab195b82
Dave Kubasiak <david.kubasiak@itron.com>
2017-11-01 11:04:04 -0400

    Made suggested changes
     - added a `context` block around test that need to call `#power` first
     - added a `before` block to that context
     - moved the channel looping tests into their respective channel_up/down test blocks
    
    [#152207328]

--- objects_tdd/spec/lib/television_spec.rb
+++ objects_tdd/spec/lib/television_spec.rb
@@ -11,98 +11,91 @@ describe Television do
     end
   end
 
-  describe '#power' do
-    it 'toggles power on and off' do
+  context '#power is turned on for these tests' do
+    before(:each) do
       subject.power
-      expect(subject.on?).to eq(true)
-      subject.power
-      expect(subject.on?).to eq(false)
-    end
-  end
-
-  describe '#set_channel(number)' do
-    it 'sets the channel to the number if within valid range' do
-      subject.power
-      subject.channel_set(45)
-      expect(subject.channel).to eq(45)
     end
 
-    it 'leaves the channel unchanged if outside valid range' do
-      subject.power
-      subject.channel_set(874)
-      expect(subject.channel).to eq(3)
-    end
-  end
-
-  describe '#channel_up' do
-    it 'increments the channel by one' do
-      subject.power
-      subject.channel_up
-      expect(subject.channel).to eq(4)
-    end
-  end
-
-  describe '#channel_down' do
-    it 'decrements the channel by one' do
-      subject.power
-      subject.channel_set(37)
-      subject.channel_down
-      expect(subject.channel).to eq(36)
-    end
-  end
-
-  describe 'channel loops when end of range is reached' do
-    it '#channel_up loops to first channel after reaching last channel' do
-      subject.power
-      subject.channel_set(500)
-      subject.channel_up
-      expect(subject.channel).to eq(3)
+    describe '#power' do
+      it 'toggles power on and off' do
+        expect(subject.on?).to eq(true)
+        subject.power
+        expect(subject.on?).to eq(false)
+      end
     end
 
-    it '#channel_down loops to last channel after reaching first channel' do
-      subject.power
-      subject.channel_down
-      expect(subject.channel).to eq(500)
-    end
-  end
+    describe '#set_channel(number)' do
+      it 'sets the channel to the number if within valid range' do
+        subject.channel_set(45)
+        expect(subject.channel).to eq(45)
+      end
 
-  describe '#channel_swap' do
-    it 'sets channel to previous channel' do
-      subject.power
-      subject.channel_set(37)
-      subject.channel_set(300)
-      subject.channel_swap
-      expect(subject.channel).to eq(37)
-      subject.channel_swap
-      expect(subject.channel).to eq(300)
+      it 'leaves the channel unchanged if outside valid range' do
+        subject.channel_set(874)
+        expect(subject.channel).to eq(3)
+      end
     end
-  end
 
-  describe '#volume_up' do
-    it 'increments the volume by one' do
-      subject.power
-      5.times { subject.volume_up }
-      expect(subject.volume).to eq(5)
+    describe '#channel_up' do
+      it 'increments the channel by one' do
+        subject.channel_up
+        expect(subject.channel).to eq(4)
+      end
+
+      it 'loops to first channel after reaching last channel' do
+        subject.channel_set(500)
+        subject.channel_up
+        expect(subject.channel).to eq(3)
+      end
     end
-  end
 
-  describe '#volume_down' do
-    it 'decrements the volume by one' do
-      subject.power
-      5.times { subject.volume_up }
-      subject.volume_down
-      expect(subject.volume).to eq(4)
+    describe '#channel_down' do
+      it 'decrements the channel by one' do
+        subject.channel_set(37)
+        subject.channel_down
+        expect(subject.channel).to eq(36)
+      end
+
+      it 'loops to last channel after reaching first channel' do
+        subject.channel_down
+        expect(subject.channel).to eq(500)
+      end
     end
-  end
 
-  describe '#mute' do
-    it 'toggles volume between 0 and whatever it was previously set to' do
-      subject.power
-      5.times { subject.volume_up }
-      subject.mute
-      expect(subject.volume).to eq(0)
-      subject.mute
-      expect(subject.volume).to eq(5)
+    describe '#channel_swap' do
+      it 'sets channel to previous channel' do
+        subject.channel_set(37)
+        subject.channel_set(300)
+        subject.channel_swap
+        expect(subject.channel).to eq(37)
+        subject.channel_swap
+        expect(subject.channel).to eq(300)
+      end
+    end
+
+    describe '#volume_up' do
+      it 'increments the volume by one' do
+        5.times { subject.volume_up }
+        expect(subject.volume).to eq(5)
+      end
+    end
+
+    describe '#volume_down' do
+      it 'decrements the volume by one' do
+        5.times { subject.volume_up }
+        subject.volume_down
+        expect(subject.volume).to eq(4)
+      end
+    end
+
+    describe '#mute' do
+      it 'toggles volume between 0 and whatever it was previously set to' do
+        5.times { subject.volume_up }
+        subject.mute
+        expect(subject.volume).to eq(0)
+        subject.mute
+        expect(subject.volume).to eq(5)
+      end
     end
   end
 end
